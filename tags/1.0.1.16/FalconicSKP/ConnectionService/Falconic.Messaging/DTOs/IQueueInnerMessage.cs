345772a18e56ddd9dc1b9a70b0d35ac31a8c0d98
ELCH <ELCH@3334ad88-a2cf-2f43-97a0-9e62a54013ab>
2019-04-05 08:53:39 +0000

    git-svn-id: https://www.aquarius.co.at/svn/Poettinger/Falconic@278 3334ad88-a2cf-2f43-97a0-9e62a54013ab

---
+++ tags/1.0.1.16/FalconicSKP/ConnectionService/Falconic.Messaging/DTOs/IQueueInnerMessage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Falconic.Messaging.DTOs
+{
+    public interface IQueueInnerMessage
+    {
+        string Type { get; }
+    }
+}
